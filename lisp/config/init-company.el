f134bef5a08ac888e1175c6e0194130b9d3c5bbb
miire <miire>
2022-07-04 20:23:27 +0800

    Add company

---
+++ lisp/config/init-company.el
@@ -0,0 +1,15 @@
+;;; init-company.el --- company config
+;;; Commentary:
+
+;;; Code:
+
+(require 'company)
+
+(add-hook 'prog-mode-hook 'global-company-mode)
+
+(setq company-minimum-prefix-length 1)
+
+
+(provide 'init-company)
+
+;;; init-company.el ends here
