e85155efb9beb009f4859f9f51b19952c7abf68e
miire <miire>
2022-07-04 22:34:12 +0800

    add flymake

--- lisp/config/init-flymake.el
+++ lisp/config/init-flymake.el
@@ -3,6 +3,10 @@
 
 ;;; Code:
 
+(add-hook 'prog-mode-hook 'flymake-mode)
+(global-set-key (kbd "M-n") #'flymake-goto-next-error)
+(global-set-key (kbd "M-p") #'flymake-goto-prev-error)
+
 (provide 'init-flymake)
 
 ;;; init-flymake.el ends here
