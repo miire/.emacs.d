7278ca23c973c8d8a7af663f18253bb90b1e9086
miire <miire>
2022-07-07 02:00:33 +0800

    Update config

--- lisp/config/config.el
+++ lisp/config/config.el
@@ -5,26 +5,21 @@
 
 (require 'init-basic)
 (require 'init-ui)
-
 (require 'init-company)
-
 (require 'init-flymake)
-
 (require 'init-markdown)
-
 (require 'init-yasnippet)
-
 (require 'init-lsp-bridge)
 
+
+(require 'init-org)
+
 (require 'init-packages)
-
 (require 'init-completion)
-
 (require 'init-tools)
 
 
 
-(require 'init-org)
 
 
 (provide 'config)
